a89c877a1d756fbb2fc9822890adb8d81466ee05
kgarcia1 <Kimmy@Kimberlys-MacBook-Pro.local>
2017-01-31 15:21:54 -0600

    Calculator Project

--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -19,11 +19,71 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(int) add:(int) firstNumber secondNumber:(int) secondNumber {
+    
+    int result = firstNumber + secondNumber;
+    return result;
 }
 
+-(IBAction) addButtonPressed {
+    int no1 = self.firstNumberTextField.text.intValue;
+    int no2 = self.secondNumberTextField.text.intValue;
+    int result = [self add: no1 secondNumber: no2];
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",result];
+    
+}
+
+-(int) subtract:(int) firstNumber secondNumber:(int) secondNumber {
+    
+    int result = firstNumber - secondNumber;
+    return result;
+    
+}
+
+-(IBAction) subtractButtonPressed {
+    int no1 = self.firstNumberTextField.text.intValue;
+    int no2 = self.secondNumberTextField.text.intValue;
+    int result = [self subtract: no1 secondNumber: no2];
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",result];
+    
+}
+
+-(int) multiply:(int) firstNumber secondNumber:(int) secondNumber {
+    
+    int result = firstNumber * secondNumber;
+    return result;
+}
+
+-(IBAction) multiplyButtonPressed {
+    int no1 = self.firstNumberTextField.text.intValue;
+    int no2 = self.secondNumberTextField.text.intValue;
+    int result = [self multiply: no1 secondNumber: no2];
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",result];
+
+}
+
+-(int) divide:(int) firstNumber secondNumber:(int) secondNumber {
+    
+    int result = firstNumber / secondNumber;
+    return result;
+}
+
+-(IBAction) divideButtonPressed {
+    int no1 = self.firstNumberTextField.text.intValue;
+    int no2 = self.secondNumberTextField.text.intValue;
+    int result = [self divide: no1 secondNumber: no2];
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",result];
+    
+}
+
+-(IBAction) equalButtonPressed {
+    int no1 = self.firstNumberTextField.text.intValue;
+    int no2 = self.secondNumberTextField.text.intValue;
+    int result = [self divide: no1 secondNumber: no2];
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",result];
+    
+}
 
 @end
