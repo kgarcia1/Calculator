83cf783f04421373bf7d1759e103ead5334dd8d9
kgarcia1 <garcia.kimlorena@gmail.com>
2017-01-31 11:45:44 -0600

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Kimberly Garcia  on 1/31/17.
+//  Copyright © 2017 Kimberly Garcia . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Kimberly Garcia  on 1/31/17.
+//  Copyright © 2017 Kimberly Garcia . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
